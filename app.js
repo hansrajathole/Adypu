0b9333e912e7f3c9aeeef46465cc33aec0278033
hansrajathole <hansrajathole1@gmail.com>
2025-07-09 06:06:43 +0530

    ADPYU college fisrt day and second day code

---
+++ app.js
@@ -0,0 +1,55 @@
+// FIRST DAY
+// WHAT IS HOISTING AND TDZ, HOW ITS WORK?
+
+// console.log(a);
+// let a = 10
+
+// fun()
+// function fun() {
+//     console.log("hoist hua");
+    
+// }
+
+
+// higher order function
+// let fun = function(sum) {
+//     let b = 20
+//     // console.log(b);
+    
+//     console.log(b);
+//     // let ans = 
+//     console.log(sum());
+    
+    
+// }
+
+// fun(sum)
+
+// function sum() {
+//     return 10+20
+// }
+// var a = 10; 
+// let c = 30
+// // let c = 40
+// const b = 
+
+// setTimeout(()=>{
+//     console.log("ho gya");
+    
+// },1000)
+
+
+
+
+//SECOND DAY
+// WHAT IS CLOSER 
+// function outer() {
+//     let a = 20 
+
+//     return function inner() {
+//         console.log(a);
+//     }
+// }
+
+// let ans = outer()
+// ans()
